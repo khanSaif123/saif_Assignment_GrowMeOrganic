38d59e9294f34dea60cc2cf8d7a63257038d3b98
khanSaif123 <khansaiflko0@gmail.com>
2024-09-13 14:33:23 +0530

    latest

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';     
+import { PrimeReactProvider } from 'primereact/api';     
 import App from './App.tsx'
 import './index.css'
 
